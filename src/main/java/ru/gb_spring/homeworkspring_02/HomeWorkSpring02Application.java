94b378575f5b196fe1193170d11dfd7abbe3969c
IliaBu <bunamaksi@yandex.ru>
2024-04-06 17:19:50 +0500

    Create code

---
+++ src/main/java/ru/gb_spring/homeworkspring_02/HomeWorkSpring02Application.java
@@ -0,0 +1,13 @@
+package ru.gb_spring.homeworkspring_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpring02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSpring02Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb_spring/homeworkspring_02/HomeWorkSpring02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb_spring.homeworkspring_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSpring02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
