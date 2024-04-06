656b22ec6215ff4971cf422e25489fdbe89eaace
IliaBu <117571487+IliaBu@users.noreply.github.com>
2024-04-06 17:22:27 +0500

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,12 @@
-CREATE TABLE IF NOT EXISTS userTable (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS userTable;
+CREATE TABLE userTable
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
     firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL
+    lastName  varchar(50) NOT NULL
 );
+
+INSERT INTO userTable (firstName, lastName)
+VALUES ('Иван', 'Иванов'),
+       ('Сергей', 'Крапивин'),
+       ('Руслан', 'Иванов');
