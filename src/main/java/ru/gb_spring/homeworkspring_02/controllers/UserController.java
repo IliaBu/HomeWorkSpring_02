d00f528935565aa729c6c4fe3ce2f5de30a4a415
IliaBu <117571487+IliaBu@users.noreply.github.com>
2024-04-06 17:28:27 +0500

    Update UserController.java

--- src/main/java/ru/gb_spring/homeworkspring_02/controllers/UserController.java
+++ src/main/java/ru/gb_spring/homeworkspring_02/controllers/UserController.java
@@ -1,20 +1,24 @@
-package ru.gb_spring.homeworkspring_02.controller;
+package ru.gb_spring.homeworkspring_02.controllers;
 
+import ru.gb_spring.homeworkspring_02.model.User;
+import ru.gb_spring.homeworkspring_02.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.gb_spring.homeworkspring_02.model.User;
-import ru.gb_spring.homeworkspring_02.service.UserService;
 
 import java.util.List;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     /** Перенаправление c корня сайта на главную страницу "/" на "/html"
      * @return страницу
@@ -29,17 +33,19 @@ public class UserController {
      * @return список пользователей
      */
     @GetMapping("/users")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "user-list";
+        //return "home.html";
     }
 
     /** Страница с вводом данных нового пользователя
+     * @param user
      * @return страницу
      */
     @GetMapping("/user-create")
-    public String createUserForm(){
+    public String createUserForm(User user) {
         return "user-create";
     }
 
@@ -48,8 +54,18 @@ public class UserController {
      * @return страницу со списком пользователей
      */
     @PostMapping("/user-create")
-    public String createUser(User user){
+    public String createUser(User user) {
         userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    /** Удаление пользователя по id
+     * @param id
+     * @return страницу со списком пользователей
+     */
+    @GetMapping("/user-delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteUserById(id);
         return "redirect:/users";
     }
 
@@ -74,16 +90,6 @@ public class UserController {
     @PostMapping("/user-update/{id}")
     public String updateUser(User user) {
         userService.updateUser(user);
-        return "redirect:/users";
-    }
-
-    /** Удаление пользователя по id
-     * @param id
-     * @return страницу со списком пользователей
-     */
-    @GetMapping("user-delete/{id}")
-    public String deleteUser(@PathVariable int id){
-        userService.deleteById(id);
         return "redirect:/users";
     }
 }
